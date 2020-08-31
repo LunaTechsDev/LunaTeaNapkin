e792e43f2ab8f73c739d591f037f5b139586f47c
inc0der <inc0der.code@gmail.com>
2020-08-31 13:56:38 -0300

    Fix paths to use relative paths (#4)

--- tests/test.js
+++ tests/test.js
@@ -2,19 +2,21 @@ const fs = require("fs").promises;
 const test = require("ava");
 const napkin = require("../src/main.js");
 
+const FIXTURE_DIR = './tests/fixtures'
+
 test.after(async (t) => {
-  await fs.unlink("tests\\fixtures\\declaration_temp.js");
-  await fs.unlink("tests\\fixtures\\memberExpression_temp.js");
+  await fs.unlink(`${FIXTURE_DIR}/declaration_temp.js`);
+  await fs.unlink(`${FIXTURE_DIR}/memberExpression_temp.js`);
 });
 
 test("converts literal declarations", async (t) => {
   const originalData = await fs.readFile(
-    "tests\\fixtures\\declaration.js",
+    `${FIXTURE_DIR}/declaration.js`,
     "utf8"
   );
-  await fs.writeFile("tests\\fixtures\\declaration_temp.js", originalData);
+  await fs.writeFile(`${FIXTURE_DIR}/declaration_temp.js`, originalData);
   const tempFile = await fs.readFile(
-    "tests\\fixtures\\declaration_temp.js",
+    `${FIXTURE_DIR}/declaration_temp.js`,
     "utf8"
   );
   const result = napkin.parse(tempFile);
@@ -23,12 +25,12 @@ test("converts literal declarations", async (t) => {
 
 test("converts literal member expressions", async (t) => {
   const originalData = await fs.readFile(
-    "tests\\fixtures\\memberExpression.js",
+    `${FIXTURE_DIR}/memberExpression.js`,
     "utf8"
   );
-  await fs.writeFile("tests\\fixtures\\memberExpression_temp.js", originalData);
+  await fs.writeFile(`${FIXTURE_DIR}/memberExpression_temp.js`, originalData);
   const tempFile = await fs.readFile(
-    "tests\\fixtures\\memberExpression_temp.js",
+    `${FIXTURE_DIR}/memberExpression_temp.js`,
     "utf8"
   );
   const result = napkin.parse(tempFile);
