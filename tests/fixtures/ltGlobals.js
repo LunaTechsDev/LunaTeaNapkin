812c9bea28ac6902aaec27285cd5bb0dc77226c5
inc0der <inc0der.code@gmail.com>
2020-09-01 09:33:45 -0300

    Add tests for LTGlobals call expressions

--- tests/fixtures/ltGlobals.js
+++ tests/fixtures/ltGlobals.js
@@ -4,3 +4,6 @@ let uiAreaWidth = _$LTGlobals_$.$dataSystem.advanced.uiAreaWidth;
 // Variable declaration w/ call expression
 let padding = _$LTGlobals_$.$gameSystem.windowPadding()
 
+// Call expressions
+_$LTGlobals_$.$gameSystem.windowPadding();
+_$LTGlobals_$.$gameSwitches.setValue(data.id, data.value);
