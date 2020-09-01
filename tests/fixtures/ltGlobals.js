16ce55dcada47821746e018c85c65ac86eaeab86
inc0der <inc0der.code@gmail.com>
2020-08-31 23:22:17 -0300

    Add unit tests for variable call expressions

--- tests/fixtures/ltGlobals.js
+++ tests/fixtures/ltGlobals.js
@@ -1,2 +1,6 @@
 // Variable declaration w/ member expression
 let uiAreaWidth = _$LTGlobals_$.$dataSystem.advanced.uiAreaWidth;
+
+// Variable declaration w/ call expression
+let padding = _$LTGlobals_$.$gameSystem.windowPadding()
+
