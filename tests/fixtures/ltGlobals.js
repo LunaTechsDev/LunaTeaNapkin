804d24777fd048fb9c3a0976c383e6574afd3b89
inc0der <inc0der.code@gmail.com>
2020-09-19 12:46:38 -0300

    Add tests and update snapshots

--- tests/fixtures/ltGlobals.js
+++ tests/fixtures/ltGlobals.js
@@ -4,6 +4,9 @@ let uiAreaWidth = _$LTGlobals_$.$dataSystem.advanced.uiAreaWidth;
 // Variable declaration w/ call expression
 let padding = _$LTGlobals_$.$gameSystem.windowPadding()
 
+// Variable declaration
+let _g2 = _$LTGlobals_$.$plugins;
+
 // Call expressions
 _$LTGlobals_$.$gameSystem.windowPadding();
 _$LTGlobals_$.$gameSwitches.setValue(data.id, data.value);
