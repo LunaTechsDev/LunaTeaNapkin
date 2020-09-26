e73bb792a5cba85da9617b081cbae274f01237bf
inc0der <inc0der.code@gmail.com>
2020-09-26 13:59:57 -0300

    Remove ignore and allow list

--- src/referenceTracker.js
+++ src/referenceTracker.js
@@ -2,16 +2,10 @@ import * as tt from "@babel/types";
 import ReferenceCounter from "./transforms/utils/ReferenceCounter";
 export const classRefTracker = new ReferenceCounter("Classes");
 
-const ignoreList = ["Main"];
-
 export function referenceTracker(path) {
   if (tt.isNewExpression(path.node)) {
     const { callee } = path.node;
-    const shouldIgnore = ignoreList.some((i) => i === callee.name);
-
-    if (shouldIgnore === false) {
-      classRefTracker.addReference(callee.name, path);
-    }
+    classRefTracker.addReference(callee.name, path);
   }
   if (tt.isMemberExpression(path.node.callee)) {
     const name = path.node.callee?.object?.name;
--- src/transforms/removeUnusedClasses.js
+++ src/transforms/removeUnusedClasses.js
@@ -1,17 +1,10 @@
 import * as tt from "@babel/types";
 import { classRefTracker } from "../referenceTracker";
 
-const allowList = [
-  'Main'
-]
-
 export default function removeUnusedClasses(path) {
   const { node } = path;
 
   if (tt.isClassDeclaration(node)) {
-    if (allowList.some(i => i === node.id.name)) {
-      return;
-    }
       if (!classRefTracker.has(node.id.name)) {
         path.remove();
       }
