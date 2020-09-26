3ce5262ae09a760f1837d20de5c1b5fe34349e9d
inc0der <inc0der.code@gmail.com>
2020-09-26 10:03:04 -0300

    Add optional chain to conditonal in nestedToPropExpr

--- src/transforms/nestedToPropExpr.js
+++ src/transforms/nestedToPropExpr.js
@@ -1,8 +1,8 @@
 import * as tt from "@babel/types";
 
-export default function nestedToPropertyExpr(init) {
+export default function nestedToPropExpr(init) {
   const { object } = init.object;
-  if (object.object && object.object.name === "_$LTGlobals_$") {
+  if (object?.object && object.object.name === "_$LTGlobals_$") {
     const { property, object } = init;
     const newObject = tt.identifier(object.object.property.name);
     const newExpression = tt.memberExpression(
