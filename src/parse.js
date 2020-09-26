a917bce8bfc989ef654b4a96e6144a51b9a4e7fe
inc0der <inc0der.code@gmail.com>
2020-09-26 18:22:29 -0300

    Add check for existing path in reference

--- src/parse.js
+++ src/parse.js
@@ -29,7 +29,7 @@ export default function parse(code, usePretty = true) {
 
       const refs = classRefTracker.getReferences();
       for (let [key, value] of refs) {
-        if (value.count <= 1 && tt.isClassDeclaration(value.path.node)) {
+        if (value.count <= 1 && value.path && tt.isClassDeclaration(value.path.node)) {
           value.path.remove();
         }
       }
