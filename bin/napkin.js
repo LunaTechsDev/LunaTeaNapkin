ba02347024c37c4ade3723528e3208a88b62f377
inc0der <inc0der.code@gmail.com>
2020-09-26 10:03:36 -0300

    Compile latest changes to bin

--- bin/napkin.js
+++ bin/napkin.js
@@ -1,102 +1,25 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
-const prettier = require("prettier");
+Object.defineProperty(exports, '__esModule', { value: true });
 
-const traverse = require("@babel/traverse").default;
+var babelTraverse = require('@babel/traverse');
+var babelGenerator = require('@babel/generator');
+var tt = require('@babel/types');
+var prettier = require('prettier');
+var yargs = require('yargs');
+var fs = require('fs/promises');
+var path = require('path');
 
-const generate = require("@babel/generator").default;
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-const tt = require("@babel/types");
+var babelTraverse__default = /*#__PURE__*/_interopDefaultLegacy(babelTraverse);
+var babelGenerator__default = /*#__PURE__*/_interopDefaultLegacy(babelGenerator);
+var prettier__default = /*#__PURE__*/_interopDefaultLegacy(prettier);
+var fs__default = /*#__PURE__*/_interopDefaultLegacy(fs);
+var path__default = /*#__PURE__*/_interopDefaultLegacy(path);
 
-const {
-  argv
-} = require("yargs");
-
-const fs = require("fs").promises;
-
-const path = require('path');
-
-const TARGET_DIR = argv.path ? path.resolve(argv.path) : path.resolve('dist');
-const usePretty = argv.pretty === undefined ? true : argv.pretty;
-
-const buildComment = filename => {
-  return `/** ============================================================================
- *
- *  ${filename}
- * 
- *  Build Date: ${new Date().toLocaleDateString("en-US")}
- * 
- *  Made with LunaTea -- Haxe
- *
- * =============================================================================
-*/
-`;
-};
-
-if (require.main === module) {
-  /**
-   * The CLI program
-   */
-  (async function main() {
-    const paths = await fs.readdir(TARGET_DIR);
-    paths.forEach(async path => {
-      const data = await fs.readFile(`${TARGET_DIR}/${path}`, {
-        encoding: "utf8"
-      });
-      const result = parse(data, usePretty);
-      await fs.writeFile(`${TARGET_DIR}/${path}`, buildComment(path) + result, {
-        encoding: "utf8"
-      });
-    });
-  })().catch(error => {
-    process.exitCode = 1;
-    console.error(error);
-  });
-}
-/**
- * Parses the code with prettier and applies specific transformation for the
- * output of plugins developed with LunaTea.
- *
- * @param {String} code The code to transform and prettify.
- * @param {Bool} usePretty Set to false to disable the use of pretty on the transformed code
- */
-
-
-function parse(code, usePretty = true) {
-  return prettier.format(code, {
-    parser(text, {
-      babel
-    }) {
-      const ast = babel(text);
-      cleanComments(ast.comments);
-      traverse(ast, {
-        enter(path) {
-          removeEmptyClasses(path);
-          removeUnwantedIdentifier(path);
-          convertPrototypeLiteralsToObject(path.node);
-        }
-
-      }); // Run generated code through prettier's default parser for even cleaner code
-
-      const codeTransformations = generate(ast, {
-        retainLines: true
-      }).code;
-
-      if (usePretty === false) {
-        return codeTransformations;
-      }
-
-      return prettier.format(codeTransformations, {
-        endOfLine: "lf",
-        parser: "babel"
-      });
-    }
-
-  });
-}
-
-function cleanComments(comments) {
+function cleanCommentSymbols(comments) {
   const lineComments = comments.filter(c => c.type === "CommentLine");
   lineComments.forEach(comment => {
     if (comment.value.includes("@")) {
@@ -105,63 +28,95 @@ function cleanComments(comments) {
   });
 }
 
-function removeUnwantedIdentifier(path) {
+function removeEmptyClasses(path, onRemove) {
   const {
     node
   } = path;
 
-  if (tt.isVariableDeclaration(node)) {
-    if (isVariableWithMemberExpression(node)) {
-      const {
-        init
-      } = node.declarations[0];
-      const newExpression = nestedToPropertyExpression(init);
-
-      if (tt.isMemberExpression(newExpression)) {
-        node.declarations[0].init.object = newExpression;
+  if (tt.isClassDeclaration(node)) {
+    if (node.body.body.length <= 0) {
+      if (typeof onRemove === "function") {
+        onRemove(node.id);
       }
-    } else if (isVariableWithCallExpression(node)) {
-      const {
-        init
-      } = node.declarations[0];
-      const newExpression = nestedToCallExpression(init.callee);
 
-      if (tt.isMemberExpression(newExpression)) {
-        node.declarations[0].init.callee = newExpression;
-      }
-    } else if (isVariableWithMemberIdentifier(node)) {
-      const declaration = node.declarations[0];
-      const iss = isVariableWithMemberIdentifier(node);
-      const newExpression = memberExpressionToExpression(declaration);
-
-      if (newExpression) {
-        node.declarations.splice(0, 1, newExpression);
-      }
-    }
-  }
-
-  if (tt.isCallExpression(node)) {
-    convertCallExpressionArguments(node);
-    const {
-      callee
-    } = node;
-
-    if (tt.isMemberExpression(callee === null || callee === void 0 ? void 0 : callee.object)) {
-      var _object$object;
-
-      const {
-        object
-      } = callee;
-
-      if ((object === null || object === void 0 ? void 0 : (_object$object = object.object) === null || _object$object === void 0 ? void 0 : _object$object.name) === "_$LTGlobals_$") {
-        const newExpression = nestedToCallExpression(callee);
-        node.callee = newExpression;
-      }
+      path.remove();
     }
   }
 }
 
-function convertCallExpressionArguments(node) {
+function isVarMemberExpr(node) {
+  const {
+    declarations
+  } = node;
+
+  if (declarations) {
+    const {
+      init
+    } = declarations[0];
+    return init && tt.isMemberExpression(init) && tt.isMemberExpression(init.object);
+  }
+
+  return false;
+}
+
+function isVarMemberIdentifier(node) {
+  const {
+    declarations
+  } = node;
+
+  if (declarations) {
+    const {
+      init
+    } = declarations[0];
+    return init && tt.isMemberExpression(init) && tt.isIdentifier(init.object) && tt.isIdentifier(init.property);
+  }
+
+  return false;
+}
+
+function varMemberExprToExpr(declaration) {
+  const {
+    id,
+    init
+  } = declaration;
+
+  if (init && init.object && init.object.name === "_$LTGlobals_$") {
+    const {
+      property,
+      object
+    } = init;
+    const newExpression = tt.variableDeclarator(tt.identifier(id.name), tt.identifier(property.name));
+    return newExpression;
+  }
+}
+
+function nestedToPropExpr(init) {
+  const {
+    object
+  } = init.object;
+
+  if ((object === null || object === void 0 ? void 0 : object.object) && object.object.name === "_$LTGlobals_$") {
+    const {
+      property,
+      object
+    } = init;
+    const newObject = tt.identifier(object.object.property.name);
+    const newExpression = tt.memberExpression(newObject, object.property, false);
+    return newExpression;
+  }
+}
+
+function nestedToCallExpr(callee) {
+  var _callee$object, _callee$object$object;
+
+  if ((callee === null || callee === void 0 ? void 0 : (_callee$object = callee.object) === null || _callee$object === void 0 ? void 0 : (_callee$object$object = _callee$object.object) === null || _callee$object$object === void 0 ? void 0 : _callee$object$object.name) === "_$LTGlobals_$") {
+    const newObject = tt.identifier(callee.object.property.name);
+    const newExpression = tt.memberExpression(newObject, callee.property, false);
+    return newExpression;
+  }
+}
+
+function convertCallExprArgs(node) {
   for (const [index, arg] of node.arguments.entries()) {
     if (tt.isCallExpression(arg)) {
       var _node$callee, _node$callee$object;
@@ -185,143 +140,63 @@ function convertCallExpressionArguments(node) {
   }
 }
 
-function isVariableWithMemberIdentifier(node) {
+// Utils
+function removeUnwantedIdentifier(path) {
   const {
-    declarations
-  } = node;
+    node
+  } = path;
 
-  if (declarations) {
-    const {
-      init
-    } = declarations[0];
-    return init && tt.isMemberExpression(init) && tt.isIdentifier(init.object) && tt.isIdentifier(init.property);
-  }
-
-  return false;
-}
-
-function isVariableWithMemberExpression(node) {
-  const {
-    declarations
-  } = node;
-
-  if (declarations) {
-    const {
-      init
-    } = declarations[0];
-    return init && tt.isMemberExpression(init) && tt.isMemberExpression(init.object);
-  }
-
-  return false;
-}
-
-function memberExpressionToExpression(declaration) {
-  const {
-    id,
-    init
-  } = declaration;
-
-  if (init && init.object && init.object.name === "_$LTGlobals_$") {
-    const {
-      property,
-      object
-    } = init;
-    const newExpression = tt.variableDeclarator(tt.identifier(id.name), tt.identifier(property.name));
-    return newExpression;
-  }
-}
-
-function nestedToCallExpression(callee) {
-  var _callee$object, _callee$object$object;
-
-  if ((callee === null || callee === void 0 ? void 0 : (_callee$object = callee.object) === null || _callee$object === void 0 ? void 0 : (_callee$object$object = _callee$object.object) === null || _callee$object$object === void 0 ? void 0 : _callee$object$object.name) === "_$LTGlobals_$") {
-    const newObject = tt.identifier(callee.object.property.name);
-    const newExpression = tt.memberExpression(newObject, callee.property, false);
-    return newExpression;
-  }
-}
-
-function nestedToPropertyExpression(init) {
-  const {
-    object
-  } = init.object;
-
-  if (object.object && object.object.name === "_$LTGlobals_$") {
-    const {
-      property,
-      object
-    } = init;
-    const newObject = tt.identifier(object.object.property.name);
-    const newExpression = tt.memberExpression(newObject, object.property, false);
-    return newExpression;
-  }
-}
-
-function isVariableWithCallExpression(node) {
-  const {
-    declarations
-  } = node;
-
-  if (declarations) {
-    const {
-      init
-    } = declarations[0];
-    return init && tt.isCallExpression(init);
-  }
-
-  return false;
-}
-/**
- * Converts prototype literal assignments and declarations
- *
- * @param {Node} node The AST node from babels parser
- * @example
- * var alias = Class.prototype['methodName'] - > var alias = Class.prototype.methodName;
- *
- * Class.prototype['methodName'] = function (){} -> Class.prototype.methodName = function (){}
- */
-
-
-function convertPrototypeLiteralsToObject(node) {
-  if (isPrototypeLiteralAssignment(node)) {
-    const {
-      property,
-      object
-    } = node.left;
-    const newLeft = literalToObject(node.left);
-    node.left = newLeft;
-  } else if (isPrototypeAliasLiteralDeclaration(node)) {
-    const {
-      init
-    } = node.declarations[0];
-    const newInit = literalToObject(init);
-    node.declarations[0].init = newInit;
-  }
-}
-/**
- * Checks if there is a variable declaration for a class prototype using bracket
- * notation.
- *
- * @param {Node} node The AST node from babels parser
- *
- * @example var alias = Class.prototype['methodName']
- */
-
-
-function isPrototypeAliasLiteralDeclaration(node) {
   if (tt.isVariableDeclaration(node)) {
-    const declaration = node.declarations[0];
-
-    if (tt.isMemberExpression(declaration.init) && tt.isLiteral(declaration.init.property)) {
+    if (isVarMemberExpr(node)) {
       const {
-        property
-      } = declaration.init.object;
-      return property && property.name === "prototype";
+        init
+      } = node.declarations[0];
+      const newExpression = nestedToPropExpr(init);
+
+      if (tt.isMemberExpression(newExpression)) {
+        node.declarations[0].init.object = newExpression;
+      }
+    } else if (isVarMemberExpr(node)) {
+      const {
+        init
+      } = node.declarations[0];
+      const newExpression = nestedToCallExpr(init.callee);
+
+      if (tt.isMemberExpression(newExpression)) {
+        node.declarations[0].init.callee = newExpression;
+      }
+    } else if (isVarMemberIdentifier(node)) {
+      const declaration = node.declarations[0];
+      const iss = isVarMemberIdentifier(node);
+      const newExpression = varMemberExprToExpr(declaration);
+
+      if (newExpression) {
+        node.declarations.splice(0, 1, newExpression);
+      }
     }
   }
 
-  return false;
+  if (tt.isCallExpression(node)) {
+    convertCallExprArgs(node);
+    const {
+      callee
+    } = node;
+
+    if (tt.isMemberExpression(callee === null || callee === void 0 ? void 0 : callee.object)) {
+      var _object$object;
+
+      const {
+        object
+      } = callee;
+
+      if ((object === null || object === void 0 ? void 0 : (_object$object = object.object) === null || _object$object === void 0 ? void 0 : _object$object.name) === "_$LTGlobals_$") {
+        const newExpression = nestedToCallExpr(callee);
+        node.callee = newExpression;
+      }
+    }
+  }
 }
+
 /**
  * Checks if there is an assignment expression for a class prototype using
  * bracket notation.
@@ -331,8 +206,7 @@ function isPrototypeAliasLiteralDeclaration(node) {
  * @example Class.prototype['methodName']
  */
 
-
-function isPrototypeLiteralAssignment(node) {
+function isProtoLiteralAssignment(node) {
   if (tt.isAssignmentExpression(node) && tt.isLiteral(node.left.property)) {
     const {
       property,
@@ -349,21 +223,30 @@ function isPrototypeLiteralAssignment(node) {
   return false;
 }
 
-function removeEmptyClasses(path, onRemove) {
-  const {
-    node
-  } = path;
+/**
+ * Checks if there is a variable declaration for a class prototype using bracket
+ * notation.
+ *
+ * @param {Node} node The AST node from babels parser
+ *
+ * @example var alias = Class.prototype['methodName']
+ */
 
-  if (tt.isClassDeclaration(node)) {
-    if (node.body.body.length <= 0) {
-      if (typeof onRemove === "function") {
-        onRemove(node.id);
-      }
+function isProtoLiteralVar(node) {
+  if (tt.isVariableDeclaration(node)) {
+    const declaration = node.declarations[0];
 
-      path.remove();
+    if (tt.isMemberExpression(declaration.init) && tt.isLiteral(declaration.init.property)) {
+      const {
+        property
+      } = declaration.init.object;
+      return property && property.name === "prototype";
     }
   }
+
+  return false;
 }
+
 /**
  * Converts a member expression from bracket notation to object notation and
  * returns a new MemberExpression with the changes.
@@ -373,7 +256,6 @@ function removeEmptyClasses(path, onRemove) {
  *
  * @example Class.prototype['methodName'] -> Class.prototype.methodName
  */
-
 
 function literalToObject(leftNode) {
   const {
@@ -385,6 +267,116 @@ function literalToObject(leftNode) {
   return newAssignment;
 }
 
-module.exports = {
-  parse
+/**
+ * Converts prototype literal assignments and declarations
+ *
+ * @param {Node} node The AST node from babels parser
+ * @example
+ * var alias = Class.prototype['methodName'] - > var alias = Class.prototype.methodName;
+ *
+ * Class.prototype['methodName'] = function (){} -> Class.prototype.methodName = function (){}
+ */
+
+function protoLiteralToObj(node) {
+  if (isProtoLiteralAssignment(node)) {
+    const {
+      property,
+      object
+    } = node.left;
+    const newLeft = literalToObject(node.left);
+    node.left = newLeft;
+  } else if (isProtoLiteralVar(node)) {
+    const {
+      init
+    } = node.declarations[0];
+    const newInit = literalToObject(init);
+    node.declarations[0].init = newInit;
+  }
+}
+
+function lunaTeaTransformer(ast, path) {
+  cleanCommentSymbols(ast.comments);
+  removeEmptyClasses(path);
+  removeUnwantedIdentifier(path);
+  protoLiteralToObj(path.node);
+}
+
+const traverse = babelTraverse__default['default'];
+const generate = babelGenerator__default['default'];
+/**
+ * Parses the code with prettier and applies specific transformation for the
+ * output of plugins developed with LunaTea.
+ *
+ * @param {String} code The code to transform and prettify.
+ * @param {Bool} usePretty Set to false to disable the use of pretty on the transformed code
+ */
+
+function parse(code, usePretty = true) {
+  return prettier__default['default'].format(code, {
+    parser(text, {
+      babel
+    }) {
+      const ast = babel(text);
+      traverse(ast, {
+        enter(path) {
+          lunaTeaTransformer(ast, path);
+        }
+
+      }); // Run generated code through prettier's default parser
+
+      const codeTransformations = generate(ast, {
+        retainLines: true
+      }).code;
+
+      if (usePretty === false) {
+        return codeTransformations;
+      }
+
+      return prettier__default['default'].format(codeTransformations, {
+        endOfLine: "lf",
+        parser: "babel"
+      });
+    }
+
+  });
+}
+
+const TARGET_DIR = yargs.argv.path ? path__default['default'].resolve(yargs.argv.path) : path__default['default'].resolve("dist");
+const usePretty = yargs.argv.pretty === undefined ? true : yargs.argv.pretty;
+
+const buildComment = filename => {
+  return `/** ============================================================================
+ *
+ *  ${filename}
+ * 
+ *  Build Date: ${new Date().toLocaleDateString("en-US")}
+ * 
+ *  Made with LunaTea -- Haxe
+ *
+ * =============================================================================
+*/
+`;
 };
+
+if (require.main === module) {
+  /**
+   * The CLI program
+   */
+  (async function main() {
+    const paths = await fs__default['default'].readdir(TARGET_DIR);
+    paths.forEach(async path => {
+      const data = await fs__default['default'].readFile(`${TARGET_DIR}/${path}`, {
+        encoding: "utf8"
+      });
+      const result = parse(data, usePretty);
+      await fs__default['default'].writeFile(`${TARGET_DIR}/${path}`, buildComment(path) + result, {
+        encoding: "utf8"
+      });
+    });
+  })().catch(error => {
+    process.exitCode = 1;
+    console.error(error);
+  });
+}
+
+exports.parse = parse;
