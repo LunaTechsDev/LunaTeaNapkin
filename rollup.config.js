f7c51f8bf6acaef2389f6202ca90913e121e95da
inc0der <inc0der.code@gmail.com>
2020-09-26 09:21:59 -0300

    Update rollup externals

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 export default {
   input: "src/main.js",
+  external: ["@babel/types", "yargs", "fs/promises", "path"],
   output: {
+    banner: "#!/usr/bin/env node",
     file: "bin/napkin.js",
     format: "cjs",
   },
