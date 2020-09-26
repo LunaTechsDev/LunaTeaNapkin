08b746731875b3be4c7f00a00615357a81c3d232
inc0der <inc0der.code@gmail.com>
2020-09-26 13:54:01 -0300

    Track member and assignment exprs

--- src/referenceTracker.js
+++ src/referenceTracker.js
@@ -1,5 +1,5 @@
 import * as tt from "@babel/types";
-import ReferenceCounter from './transforms/utils/ReferenceCounter';
+import ReferenceCounter from "./transforms/utils/ReferenceCounter";
 export const classRefTracker = new ReferenceCounter("Classes");
 
 const ignoreList = ["Main"];
@@ -10,7 +10,19 @@ export function referenceTracker(path) {
     const shouldIgnore = ignoreList.some((i) => i === callee.name);
 
     if (shouldIgnore === false) {
-      classRefTracker.addReference(callee.name);
+      classRefTracker.addReference(callee.name, path);
     }
+  }
+  if (tt.isMemberExpression(path.node.callee)) {
+    const name = path.node.callee.object.name;
+    if (name) {
+      classRefTracker.addReference(path.node.callee.object.name, path);
+    }
+  }
+  if (tt.isAssignmentExpression(path.node)) {
+        const name = path.node.callee.object.name;
+        if (name) {
+          classRefTracker.addReference(path.node.callee.object.name, path);
+        }
   }
 }
