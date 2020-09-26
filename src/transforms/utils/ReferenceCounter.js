f41543e7380c0c003e9789f7075bdd046aaafd39
inc0der <inc0der.code@gmail.com>
2020-09-26 13:54:41 -0300

    Add pass the node path when adding reference

--- src/transforms/utils/ReferenceCounter.js
+++ src/transforms/utils/ReferenceCounter.js
@@ -4,13 +4,13 @@ export default class ReferenceCounter {
     this._references = new Map();
   }
 
-  addReference(identifier) {
+  addReference(identifier, path) {
     if (this._references.has(identifier)) {
       const reference = this._references.get(identifier);
       this._references.set(identifier, { count: reference.count + 1 });
       return;
     }
-    this._references.set(identifier, { count: 1 });
+    this._references.set(identifier, { count: 1, path });
   }
 
   has(identifier) {
